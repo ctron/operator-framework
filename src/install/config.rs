2bcd8d21525bb2e446fba6a16b87698960fc5a04
Jens Reimann <jreimann@redhat.com>
2020-06-19 14:38:52 +0200

    allow creating binary and string data

--- src/install/config.rs
+++ src/install/config.rs
@@ -15,14 +15,14 @@ use k8s_openapi::ByteString;
 
 use crate::utils::UseOrCreate;
 
-pub trait AppendData<T> {
-    fn append_data<S>(&mut self, key: S, value: T)
+pub trait AppendString<T> {
+    fn append_string<S>(&mut self, key: S, value: T)
     where
         S: ToString;
 }
 
-impl<T: Into<Vec<u8>>> AppendData<T> for Secret {
-    fn append_data<S>(&mut self, key: S, value: T)
+impl<T: Into<Vec<u8>>> AppendString<T> for Secret {
+    fn append_string<S>(&mut self, key: S, value: T)
     where
         S: ToString,
     {
@@ -31,12 +31,70 @@ impl<T: Into<Vec<u8>>> AppendData<T> for Secret {
     }
 }
 
-impl<T: Into<String>> AppendData<T> for ConfigMap {
-    fn append_data<S>(&mut self, key: S, value: T)
+impl<T: Into<String>> AppendString<T> for ConfigMap {
+    fn append_string<S>(&mut self, key: S, value: T)
     where
         S: ToString,
     {
         self.data
             .use_or_create(|data| data.insert(key.to_string(), value.into()));
+    }
+}
+
+pub trait AppendBinary<T> {
+    fn append_binary<S>(&mut self, key: S, value: T)
+    where
+        S: ToString;
+}
+
+impl<T: Into<Vec<u8>>> AppendBinary<T> for Secret {
+    fn append_binary<S>(&mut self, key: S, value: T)
+    where
+        S: ToString,
+    {
+        self.data
+            .use_or_create(|data| data.insert(key.to_string(), ByteString(value.into())));
+    }
+}
+
+impl<T: Into<Vec<u8>>> AppendBinary<T> for ConfigMap {
+    fn append_binary<S>(&mut self, key: S, value: T)
+    where
+        S: ToString,
+    {
+        self.binary_data
+            .use_or_create(|data| data.insert(key.to_string(), ByteString(value.into())));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_cm_string() {
+        let mut cm: ConfigMap = Default::default();
+        cm.append_string("foo", "bar");
+    }
+
+    #[test]
+    fn test_secret_string() {
+        let mut secret: Secret = Default::default();
+        secret.append_string("foo", "bar");
+    }
+
+    #[test]
+    fn test_cm_binary() {
+        let mut cm: ConfigMap = Default::default();
+        let data = [1u8, 2u8, 3u8];
+        cm.append_binary("foo", data.clone());
+    }
+
+    #[test]
+    fn test_secret_binary() {
+        let mut secret: Secret = Default::default();
+        let data = [1u8, 2u8, 3u8];
+        secret.append_binary("foo", data.clone());
     }
 }
