d586267ca51a38c32c5ca90cabf2b5807fa2003e
Jens Reimann <jreimann@redhat.com>
2022-01-21 16:22:43 +0100

    fix: enable conditional compilation for condition

--- src/conditions/k8s.rs
+++ src/conditions/k8s.rs
@@ -1,4 +1,6 @@
-crate::condition!(k8s_openapi::apimachinery::pkg::apis::meta::v1::Condition[core]);
+k8s_openapi::k8s_if_ge_1_20! {
+    crate::condition!(k8s_openapi::apimachinery::pkg::apis::meta::v1::Condition[core]);
+}
 
 crate::condition!(k8s_openapi::api::batch::v1::JobCondition[probe]);
 crate::condition!(k8s_openapi::api::apps::v1::StatefulSetCondition);
